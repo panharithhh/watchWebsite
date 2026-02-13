876e49f4428f873a9f5528b9bca76cb2e76a9979
panharithhh <161872594+panharithhh@users.noreply.github.com>
2026-02-13 09:50:22 +0700

    fix origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,12 +8,38 @@ from routers import admin, auth, catalog, emails, search, users, watches, wishli
 
 app = FastAPI(title="API")
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origin_regex=r"^http://(localhost|127\.0\.0\.1):\d+$",
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+# app.add_middleware(
+#   CORSMiddleware,
+#   allow_origins=[
+#     "http://localhost:5173",
+#     "http://127.0.0.1:5173",
+#     "https://watch123-dusky.vercel.app",
+#   ],
+#   allow_credentials=True,
+#   allow_methods=["*"],
+#   allow_headers=["*"],
+# )
+
+# allow_origins=[
+#   "http://localhost:5173",
+#   "http://127.0.0.1:5173",
+#   "https://watch123-dusky.vercel.app",
+#   "https://watch123-55p5suake-chea-panhariths-projects.vercel.app",
+# ]
+
 app.add_middleware(
-    CORSMiddleware,
-    allow_origin_regex=r"^http://(localhost|127\.0\.0\.1):\d+$",
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+  CORSMiddleware,
+  allow_origin_regex=r"^https://.*\.vercel\.app$",
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
 )
 
 models.Base.metadata.create_all(bind=engine)
